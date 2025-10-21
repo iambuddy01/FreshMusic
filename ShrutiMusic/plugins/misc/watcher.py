069767a150fbdd21945498ddf3e1f4ba117a729b
Buddy <bgmiduniya2006@gmail.com>
2025-10-21 16:14:41 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- ShrutiMusic/plugins/misc/watcher.py
+++ ShrutiMusic/plugins/misc/watcher.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -34,15 +12,3 @@ close = 30
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
     await Nand.stop_stream_force(message.chat.id)
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
